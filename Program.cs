ce8c8341b58ec225547565587bf7395b9568c3c5
Traun1 <kapitonovdmitrij5@gmail.com>
2025-03-25 09:34:16 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace starting_monkey_to_human_path
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
